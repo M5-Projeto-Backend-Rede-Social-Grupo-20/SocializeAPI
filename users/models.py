2526b2caa04fcb0255d72099e16d8a1e6ac9b99c
vinisooo <cviniciusliramdev@gmail.com>
2023-05-02 17:17:42 -0300

    fix: remove code comments

--- users/models.py
+++ users/models.py
@@ -2,12 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-# class User(AbstractUser):
-#     connections = models.ForeignKey(
-#         "connections.Connection", related_name="received_by", on_delete=models.CASCADE
-#     )
-
-
 class User(AbstractUser):
     connection = models.ForeignKey(
         "connections.Connection",
