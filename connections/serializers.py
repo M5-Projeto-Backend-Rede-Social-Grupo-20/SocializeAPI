0b3310dd0b55d6685642cdabcd00bc18e982b72d
vinisooo <cviniciusliramdev@gmail.com>
2023-05-04 12:33:15 -0300

    fix: adjust connection and user relationships

--- connections/serializers.py
+++ connections/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from .models import Connection
 
+
 class ConnectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Connection
-        fields = ['following', 'friend', 'is_accepted', 'requested_by']
+        fields = ["following", "friend", "is_accepted", "requested_by"]
