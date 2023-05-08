917beb2a923468361dd19cd892e2c902e51ecdad
vinisooo <cviniciusliramdev@gmail.com>
2023-05-08 15:56:28 -0300

    fix: add likes_count to post serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -55,6 +55,7 @@ class CommentSerializer(serializers.ModelSerializer):
 class PostSerializer(serializers.ModelSerializer):
     access = serializers.ChoiceField(choices=AccessChoices.choices)
     posted_by = ShortUserSerializer(read_only=True)
+    likes_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
@@ -64,5 +65,9 @@ class PostSerializer(serializers.ModelSerializer):
             "content",
             "access",
             "created_at",
+            "likes_count",
         )
         read_only_fields = ("id", "created_at", "posted_by", "comments", "likes")
+
+    def get_likes_count(self, obj):
+        return obj.likes.count()
